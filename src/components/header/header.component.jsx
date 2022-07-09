4b6f53c4f307d19419982e52bb35401774b3e18b
JamesGabby <jlghfe36@gmail.com>
2022-07-09 23:08:50 +0100

    styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,42 +2,57 @@ import React from "react";
 import './header.styles.css'
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
+import { useState } from "react";
 import { setUser } from "../../features/user/userSlice";
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { GrClose } from 'react-icons/gr'
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false)
+
     const user = useSelector((state) => state.user)
     const dispatch = useDispatch()
 
-    const handleClick = () => {
+    const handleLogoutClick = () => {
         // Remove the user
         dispatch(setUser(''))
         // Refresh page so user data no longer remains
         window.location.reload(false)
     }
 
+    const handleMenuClick = () => {
+        setShowMenu(!showMenu)
+    }
+
     return (
         <div className="header-container">
             <div className="left-side">
                 <Link to='/'>
-                    <img src={process.env.PUBLIC_URL + '/thoth.png'} alt="logo" id="link" width={50} />
+                    <img src={process.env.PUBLIC_URL + '/logo.svg'} alt="logo" id="link" width={50} />
                 </Link>
             </div>
             <div className="right-side">
-                <Link to='/about'>
-                    <p id="link">About</p>
-                </Link>
-                <Link to='/journal'>
-                    <p id="link">Journal</p>
-                </Link>
-                <p id="link">Shop</p>
-                {
-                    user.length === 0 ? 
-                        <Link to='/login'>
-                            <p id="link" style={{color: 'black'}}>Login</p>
-                        </Link>
-                    :
-                        <p id="link" onClick={handleClick}>Logout</p>
-                }
+                <p id="menu" onClick={handleMenuClick}>{!showMenu ? <GiHamburgerMenu style={{fontSize: '2rem'}} /> : <GrClose style={{fontSize: '2rem', backgroundColor: 'white'}} />}</p>
+            </div>
+
+            <div className={showMenu ? 'menu-dropdown' : 'menu-dropdown-hide'}>
+                <div className="links">
+                    <Link to='/about'>
+                        <p id="link">About</p>
+                    </Link>
+                    <Link to='/journal'>
+                        <p id="link">Journal</p>
+                    </Link>
+                    <p id="link">Shop</p>
+                    {
+                        user.length === 0 ? 
+                            <Link to='/login'>
+                                <p id="link" style={{color: 'white'}}>Login</p>
+                            </Link>
+                        :
+                            <p id="link" onClick={handleLogoutClick}>Logout</p>
+                    }
+                </div>
             </div>
         </div>
     );
