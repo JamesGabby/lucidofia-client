78db4da03ebbb7f3e4785e46433cfbf5739bea37
JamesGabby <jlghfe36@gmail.com>
2022-09-09 02:10:40 +0100

    added about text

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <div>
             <Header />
-          
+            <h3>Have you ever had a dream that you were so sure was real? What if you couldn't awaken? How would you know the difference between dream and reality? </h3>
         </div>
     );
 }
