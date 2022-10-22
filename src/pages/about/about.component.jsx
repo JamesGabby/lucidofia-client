6d66c79a015f95bd7787fe380f9618d315f88fea
JamesGabby <jlghfe36@gmail.com>
2022-10-22 16:41:50 +0100

    add footer to about, change classes to functions

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import './about.styles.css'
 import Header from "../../components/header/header.component";
+import Footer from "../../components/footer/footer.component";
 
-const About = () => {
-
-    return (
-        <div>
-            <div className="about">
-                <Header />
-                <div className="about-container">
-                    <h3>Have you ever had a dream that you were so sure was real? What if you couldn't awaken? How would you know the difference between dream and reality? </h3>
-                    <p>- Morpheus, The Matrix</p>
-                </div>
-                
+const About = () => (
+    <div>
+        <div className="about">
+            <Header />
+            <div className="about-container">
+                <h3>Have you ever had a dream that you were so sure was real? What if you couldn't awaken? How would you know the difference between dream and reality? </h3>
+                <p>- Morpheus, The Matrix</p>
             </div>
         </div>
-    );
-}
+        <Footer />
+    </div>  
+)
 
 export default About;
